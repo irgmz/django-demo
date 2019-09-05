51547eebc65bed2e4dfc953047b0c288e2f570f4
atran <atran@rollbar.com>
2019-09-05 11:51:08 -0700

    fix rb#13

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,13 @@ git push
 
 REVISION=$(git rev-parse HEAD)
 
-DATA='{"access_token":"e3211e5f9042453497de0f363739b770","environment":"development","revision":"REV"}'
+DATA='{
+    "access_token":"e3211e5f9042453497de0f363739b770",
+    "environment":"development",
+    "revision":"REV"
+    "rollbar_username":"ajtran",
+    "local_username":"ajtran"
+}'
 
 FINAL="${DATA/REV/$REVISION}"
 
