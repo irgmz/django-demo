728e53e4c1001a54003e1e304c427673ccff784d
atran <atran@rollbar.com>
2019-09-05 11:46:20 -0700

    Did some work

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+git add .
+git commit -m "fix rb#13"
+git push
+
+REVISION=$(git rev-parse HEAD)
+
+DATA='{"access_token":"e3211e5f9042453497de0f363739b770","environment":"development","revision":"REV"}'
+
+FINAL="${DATA/REV/$REVISION}"
+
+curl \
+    --request POST \
+    --url https://api.rollbar.com/api/1/deploy/ \
+    --header 'content-type: application/json' \
+    --data $FINAL
